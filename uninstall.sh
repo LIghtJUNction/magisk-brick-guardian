c1d0cd25d5584d4b6b0a90f9ed4683111ee31d2c
Kirk Lin <linkirk@163.com>
2025-02-25 12:23:52 +0800

    refactor: simplify uninstall.sh to rely on Magisk's built-in uninstall process

--- uninstall.sh
+++ uninstall.sh
@@ -2,19 +2,15 @@
 # Copyright (C) 2024 Kirk Lin
 # 
 # Magisk Brick Guardian 卸载脚本
-# Version: 250117
+# Version: 250225
 
-# 清理模块文件
-cleanup_module() {
-    local module_path="/data/adb/modules/magisk-brick-guardian"
-    
-    # 删除模块目录
-    if [ -d "$module_path" ]; then
-        rm -rf "$module_path"
-    fi
-}
+# 注意：此脚本保留但不执行任何操作
+# Magisk会自动处理模块的卸载过程，包括删除模块目录
 
-# 执行清理
-cleanup_module
+# 如果将来需要在卸载时执行特殊的清理操作，可以在此处添加代码
+# 例如：删除模块在系统其他位置创建的文件、恢复系统设置等
+
+# 退出脚本
+exit 0
 
 
